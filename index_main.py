6cf03d61728313a38c87c2cefb7d2fb05764aeaa
Syed Muhammad Shoaib Naqvi <muhshoaib@yahoo.com>
2024-07-09 23:49:09 +0500

    intro queries to connect with sql database

--- index_main.py
+++ index_main.py
@@ -1,2 +1,16 @@
 from tkinter import *
 import pandas as pd
+import pypyodbc as odbc
+
+DRIVER_NAME="SQL SERVER"
+SERVER_NAME="DESKTOP-OFOE4B8\SQLEXPRESS"
+DATABASE_NAME="library"
+connection_string=f"""
+    DRIVER={{{DRIVER_NAME}}};
+    SERVER={{{SERVER_NAME}}};
+    DATABASE={{{DATABASE_NAME}}};
+    Trust_Connection=yes;
+"""
+conn=odbc.connect(connection_string)
+print(conn)
+
